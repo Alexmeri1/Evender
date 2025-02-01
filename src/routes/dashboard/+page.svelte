6ea5bca408da5b51a6c86d7fe239023b41a94a65
paulinadupin <paulinaguayo88@gmail.com>
2025-02-01 15:07:18 -0500

    hello2

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,21 +1,23 @@
 <script lang='ts'>
-  import "./styles.css"
+  import "./styles.css";
 </script>
 
-<head>
+<svelte:head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=devide-width, initial-scale=1.0">
+  <meta name="viewport" content="initial-scale=1.0">
+  
   <title>Dashboard</title>
-  <!-- <link... CSS -->
-</head>
+</svelte:head>
+
 <body>
   <div class="container">
+
     <img class="card-image" src="#" alt="Card Image">
+
     <h2 class="card-title">Card Title</h2>
     <p class="card-details">Location</p>
     <p class="card-details">Details</p>
   </div>
-  <button class="nav right"></button>
 
 
   <div class="sidebar">DECKS</div>

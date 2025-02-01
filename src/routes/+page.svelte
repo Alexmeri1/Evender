6d6244a777ad0cf4910f00d46d0cee4108a9c54c
Nickeldon <50726216+Nickeldon@users.noreply.github.com>
2025-02-01 17:33:22 -0500

    yes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import '$lib/resources/components/partyBG.scss';
+    import PartyScreen from '$lib/resources/components/partyBG.svelte';
+</script>
 
-<div id="main">
-    
+<div id="main" style="width: 100%; height: auto; display:block; justify-content: center; align-items: center; background-color: transparent; padding:0; margin:0;">
+    <div id="main-parents" style="width: 100%; height:1080px; display:block; flex-direction: row; margin:0; padding:0;">
+        <PartyScreen/>
+    </div>
 </div>
